c8f6075fe60207558b6e1ceec9fe66975ba4511c
v1nter <sebastiankleinhaltern@gmail.com>
2021-03-09 22:35:17 +0100

    Commit

--- Backend/settings.py
+++ Backend/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=4ck+6gg!zi#-lm)k56+!31n5^wmegy4#2j8v#48c(6@yr3d+='
+# SECRET_KEY = '=4ck+6gg!zi#-lm)k56+!31n5^wmegy4#2j8v#48c(6@yr3d+='
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '=4ck+6gg!zi#-lm)k56+!31n5^wmegy4#2j8v#48c(6@yr3d+=')
 
 ################################################################################
 #
--- zzz_CMD.bat
+++ zzz_CMD.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /k "cd /d C:\Users\Sebastian\Envs\bbcode\Scripts & activate & cd /d C:\Users\Sebastian\python_projects\git\BBCode\Backend"
+cmd /k "cd /d C:\Users\Sebastian\Envs\bbcode\Scripts & activate & cd /d C:\Users\Sebastian\python_projects\git\BBCodeBackend"
